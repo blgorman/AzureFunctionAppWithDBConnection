8e9cbd028b4a5daa6e0f25ab5c50909c5b539a83
Brian L. Gorman <blgorman@gmail.com>
2026-02-11 11:41:58 -0600

    updates

--- FunctionAppWithDatabaseAndEFCore/FunctionAppWithDatabaseAndEFCore/Program.cs
+++ FunctionAppWithDatabaseAndEFCore/FunctionAppWithDatabaseAndEFCore/Program.cs
@@ -2,6 +2,7 @@ using DataLayer;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,7 +10,7 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-var connectionString = builder.Configuration["SqlConnectionString"];
+var connectionString = builder.Configuration.GetConnectionString("SqlConnectionString");
 
 builder.Services
     .AddDbContext<FunctionAppDbContext>(options => options.UseSqlServer(connectionString))
